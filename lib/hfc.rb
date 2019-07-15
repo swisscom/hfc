9065e82e1b969c2363e4d3f6a8fe13d8f4fa3a34
Fabian Stillhart <fabian.stillhart1@swisscom.com>
2019-07-15 14:32:32 +0200

    change line length of initialize

--- lib/hfc.rb
+++ lib/hfc.rb
@@ -13,7 +13,12 @@ module HFC_Base
   attr_accessor :config, :facts
   attr_accessor :lookup_paths
 
-  def initialize(lookup_paths: ENV['HFC'] ? ENV['HFC'].split(',') : ['/opt/hfc', File.join(ENV['HOME'].to_s, '.config', 'hfc')], config: ::ActiveSupport::HashWithIndifferentAccess.new, auto_lookup: true, facts: ActiveSupport::HashWithIndifferentAccess.new)
+  def initialize(
+      lookup_paths: ENV['HFC'] ? ENV['HFC'].split(',') : ['/opt/hfc', File.join(ENV['HOME'].to_s, '.config', 'hfc')],
+      config: ::ActiveSupport::HashWithIndifferentAccess.new,
+      facts: ActiveSupport::HashWithIndifferentAccess.new
+      auto_lookup: true,
+    )
     self.config = ::ActiveSupport::HashWithIndifferentAccess.new(config)
     self.facts = ActiveSupport::HashWithIndifferentAccess.new(facts)
     @lookup_paths = lookup_paths
