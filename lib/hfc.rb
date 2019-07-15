5717a17a007fc74117d6f1c4c39d82529dd64579
Fabian Stillhart <fabian.stillhart1@swisscom.com>
2019-07-15 14:38:24 +0200

    fix bug trailing ,

--- lib/hfc.rb
+++ lib/hfc.rb
@@ -17,7 +17,7 @@ module HFC_Base
       lookup_paths: ENV['HFC'] ? ENV['HFC'].split(',') : ['/opt/hfc', File.join(ENV['HOME'].to_s, '.config', 'hfc')],
       config: ::ActiveSupport::HashWithIndifferentAccess.new,
       facts: ActiveSupport::HashWithIndifferentAccess.new,
-      auto_lookup: true,
+      auto_lookup: true
     )
     self.config = ::ActiveSupport::HashWithIndifferentAccess.new(config)
     self.facts = ActiveSupport::HashWithIndifferentAccess.new(facts)
